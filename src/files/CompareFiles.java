beb1a61d27e31d81676d2708ca767ca484b92bde
Vladislav Pavlyuk <vladpavliuk@gmail.com>
2024-09-13 17:46:15 +0200

    CompareFiles, CompareFilesFabric Classes

--- src/files/CompareFiles.java
+++ src/files/CompareFiles.java
@@ -1,2 +1,56 @@
-package files;public class CompareFiles {
+package files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CompareFiles {
+    private String path1;
+    private String path2;
+
+    public CompareFiles(){}
+
+    public CompareFiles(String path1, String path2) {
+        this.path1 = path1;
+        this.path2 = path2;
+    }
+
+    public String getPath1() {
+        return path1;
+    }
+    public void setPath1(String path1) {
+        this.path1 = path1;
+    }
+    public String getPath2() {
+        return path2;
+    }
+    public void setPath2(String path2) {
+        this.path2 = path2;
+    }
+
+    public void compareFiles() {
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(path1));
+             BufferedReader reader2 = new BufferedReader(new FileReader(path2))) {
+
+            String line1, line2;
+            boolean filesAreEqual = true;
+
+            while ((line1 = reader1.readLine()) != null & (line2 = reader2.readLine()) != null) {
+                if (!line1.equals(line2)) {
+                    System.out.println("Несовпадающие строки:");
+                    System.out.println("Файл 1: " + line1);
+                    System.out.println("Файл 2: " + line2);
+                    filesAreEqual = false;
+                }
+            }
+
+            if (filesAreEqual) {
+                System.out.println("Файлы идентичны.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при чтении файлов: " + e.getMessage());
+        }
+    }
 }
+
---
+++ src/service/CompareFilesFabric.java
@@ -0,0 +1,23 @@
+package service;
+
+import files.CompareFiles;
+
+import java.util.Scanner;
+
+public class CompareFilesFabric {
+
+    public static CompareFiles runCompareFiles() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите путь к первому файлу:");
+        String path1 = scanner.nextLine();
+
+        System.out.println("Введите путь ко второму файлу:");
+        String path2 = scanner.nextLine();
+        CompareFiles compareFiles = new CompareFiles();
+        compareFiles.setPath1(path1);
+        compareFiles.setPath2(path2);
+        compareFiles.compareFiles();
+        return compareFiles;
+    }
+}
